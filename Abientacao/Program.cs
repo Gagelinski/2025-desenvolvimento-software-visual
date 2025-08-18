e159a4616a20816ee592207c6c5c75af2ddd02da
Anna Gagelinski <anna.gagelinski@gmail.com>
2025-08-18 19:33:54 -0300

    Projeto de Ambientação e Lista de Exercícios

---
+++ Abientacao/Program.cs
@@ -0,0 +1,6 @@
+﻿// Imprimir texto no console
+Console.WriteLine("Digite seu nome:");
+
+// Ler entrada do usuário armazenando em variável
+string nome = Console.ReadLine(); //sempre que houver parenteses, é um método
+
---
+++ ListaExercicio/Exe06b/Program.cs
@@ -0,0 +1,39 @@
+﻿//Aplicando a função do C# que ordena
+
+public class Vetor
+{
+    public static void Main(string[] args)
+    {
+        int tamanho = 100;
+        int[] numeros = new int[tamanho];
+
+        Random random = new Random();
+
+        for (int i = 0; i < tamanho; i++)
+        {
+            numeros[i] = random.Next(999);
+        }
+
+        Console.WriteLine(" Iniciais: ");
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write(numeros[i] + " ");
+        }
+        Console.WriteLine(" ");
+
+        //Função do C# que faz a ordenação :)
+        Array.Sort(numeros);
+
+        Console.WriteLine("Ordenados:");
+            for (int i = 0; i < tamanho; i++)
+            {
+                Console.Write(numeros[i] + " ");
+            }
+
+            Console.WriteLine(" ");
+        }
+    }
+
+
+
+
